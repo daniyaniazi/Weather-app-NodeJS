8b9f25c77d0bdf99331b4d7433b0c85544a9d617
daniyaniazi <farihadania@hotmail.com>
2021-02-25 21:38:31 +0500

    Added address arg

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const request = require('request')
 const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
+const address = process.argv[2];
 
 
-geoCode('paris', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+    console.log("Please provide address")
+}
+else {
+    geoCode(address, (error, data) => {
         if (error) {
             return console.log(error)
         }
-        console.log("data location", data.location)
-        console.log('data :', forecastData)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log("data location", data.location)
+            console.log('data :', forecastData)
+        })
+
     })
 
-})
 
-
+}
